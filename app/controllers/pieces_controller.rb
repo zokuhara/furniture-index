517ef7e00246d26292bc92b7e5c4138137bd047a
Zachary Okuhara <zacharyokuhara@Zacharys-MacBook-Pro.local>
2015-09-12 11:53:47 -0500

    Initial Commit. List page working, detail page is throwing an error. Some early styling with Bootstrap

---
+++ app/controllers/pieces_controller.rb
@@ -0,0 +1,37 @@
+require 'csv'
+class PiecesController < ApplicationController
+
+  def list
+    @pieces = fetch_pieces
+  end
+
+  def detail
+    @pieces =fetch_pieces
+    @piece = @pieces.find {|p| p.id == params[:pid].to_i}
+  end
+
+  def fetch_pieces
+    @pieces = []
+
+    CSV.foreach(Rails.root + "data/mf_inventory.csv", headers: true) do |row|
+      # fill the array
+      piece = Piece.new
+      piece.pid = row.to_h["pid"]
+      piece.item = row.to_h["item"]
+      piece.description = row.to_h["description"]
+      piece.price = row.to_h["price"]
+      piece.condition = row.to_h["condition"]
+      piece.dimension_w = row.to_h["dimension_w"]
+      piece.dimension_l = row.to_h["dimension_l"]
+      piece.dimension_h = row.to_h["dimension_h"]
+      piece.img_file = row.to_h["img_file"]
+      piece.quantity = row.to_h["quantity"]
+      piece.category = row.to_h["category"]
+
+
+      @pieces << piece
+
+    end
+    @pieces
+  end
+end
---
+++ app/models/piece.rb
@@ -0,0 +1,5 @@
+class Piece
+  attr_accessor :pid, :item, :description, :price, :condition, :dimension_w, :dimension_l,
+   :dimension_h, :img_file, :quantity, :category
+
+end
